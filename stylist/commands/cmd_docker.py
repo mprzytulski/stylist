9e0aa2b5b482357f6a434cf698d5ba5840339cf9
Daniel Ancuta <whisller@gmail.com>
2017-09-11 16:52:33 +0100

    Styling.

--- stylist/commands/cmd_docker.py
+++ stylist/commands/cmd_docker.py
@@ -11,37 +11,28 @@ cli = copy(cli_prototype)
 cli.short_help = 'Docker image helper'
 
 
-@cli.command(help="Build docker image using Dockerfile")
-@click.option("--no-tag", default=False, flag_value='no_tag', help="Do not tag build")
+@cli.command(help='Build docker image using Dockerfile')
+@click.option('--no-tag', default=False, flag_value='no_tag', help='Do not tag build')
+@click.option('--ask', is_flag=True, help='Ask which repository to build')
 @stylist_context
-def build(ctx, no_tag):
+def build(ctx, no_tag, ask):
     """
     @type ctx: stylist.cli.Context
     """
+    click.secho('Building docker container', fg='blue')
+
+    docker_files = glob.glob('{}/Dockerfile*'.format(ctx.working_dir))
+
     try:
         build_tag = datetime.now().strftime('%Y%m%d_%H%M') if not no_tag else None
 
-        click.secho('Building docker container', fg='blue')
-
-        docker_files = [p for p in glob.glob('{}/Dockerfile*'.format(ctx.working_dir))]
-
-        if len(docker_files) > 1:
-            click.secho('Which docker file would you like to build?', fg='blue')
-            for i, docker_file in enumerate(docker_files):
-                click.secho('  [{}] {}'.format(i+1, docker_file), fg='blue')
-            all_above = len(docker_files)+1
-            click.secho('  [{}] All above.'.format(all_above), fg='blue')
-
-            docker_index = click.prompt(click.style('Build', fg='blue'), default=all_above)
-            if docker_index == all_above:
-                docker_files_indexes = tuple(range(0, len(docker_files)))
-            else:
-                docker_files_indexes = (docker_index-1,)
+        if ask:
+            indexes = _ask_about_docker_files('Which docker file would you like to build?', docker_files)
         else:
-            docker_files_indexes = (0,)
+            indexes = tuple(range(0, len(docker_files)))
 
         docker = Docker(ctx)
-        for index in docker_files_indexes:
+        for index in indexes:
             try:
                 docker.build(docker_files[index], build_tag)
 
@@ -56,18 +47,20 @@ def build(ctx, no_tag):
         sys.exit(1)
 
 
-@cli.command(help="Build docker image using Dockerfile")
-@click.argument("build_tag", "Image tag which should be pushed to current repository")
+@cli.command(help='Push docker image')
+@click.argument('build_tag', 'Image tag which should be pushed to current repository')
 @stylist_context
 def push(ctx, build_tag):
     """
     @type ctx: stylist.cli.Context
     """
+    click.secho('Pushing docker container', fg='blue')
+
     try:
         docker = Docker(ctx)
         remote = docker.push(build_tag)
 
-        click.secho("Image ready: {remote}".format(remote=remote), fg="green")
+        click.secho('Image ready: {remote}'.format(remote=remote), fg='green')
     except NotADockerProjectException:
         sys.exit(1)
     except DockerException as e:
@@ -80,7 +73,7 @@ def push(ctx, build_tag):
         sys.exit(1)
 
 
-@cli.command(help="List current project images")
+@cli.command(help='List current project images')
 @stylist_context
 def images(ctx):
     """
@@ -91,3 +84,19 @@ def images(ctx):
         docker.images()
     except NotADockerProjectException:
         sys.exit(1)
+
+
+def _ask_about_docker_files(message, docker_files):
+    click.secho(message, fg='blue')
+    for i, docker_file in enumerate(docker_files):
+        click.secho('  [{}] {}'.format(i + 1, docker_file), fg='blue')
+    all_above = len(docker_files) + 1
+    click.secho('  [{}] All above.'.format(all_above), fg='blue')
+
+    docker_index = click.prompt(click.style('Build', fg='blue'), default=all_above)
+    if docker_index == all_above:
+        docker_files_indexes = tuple(range(0, len(docker_files)))
+    else:
+        docker_files_indexes = (docker_index - 1,)
+
+    return docker_files_indexes
