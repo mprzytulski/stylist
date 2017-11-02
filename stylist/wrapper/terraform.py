818f7889f03af52cf7e8d8ad7408eb2e963c02aa
Michal Przytulski <michal@przytulski.pl>
2017-11-02 20:22:07 +0000

    make module update smarter

--- stylist/wrapper/terraform.py
+++ stylist/wrapper/terraform.py
@@ -12,6 +12,9 @@ from os.path import isfile, join, isdir, exists
 import hcl
 
 import click
+import os
+
+import time
 from click import style, prompt
 from stylist.cli import logger
 from stylist.commands.cmd_check import which
@@ -35,12 +38,17 @@ class Terraform(object):
     def terraform_dir(self):
         return join(self.ctx.working_dir, "terraform")
 
+    @property
+    def tfupdate_path(self):
+        return join(self.terraform_dir, '.tfupdate')
+
     def setup(self):
         if not exists(self.terraform_dir):
             shutil.copytree(join(self.templates.destination, 'terraform'), self.terraform_dir)
 
-    def plan(self, save=False):
+    def plan(self, save=False, force_update=False):
         vars_file = self._ensure_env()
+        self._update_modules(force_update)
 
         args = ['plan']
 
@@ -48,12 +56,6 @@ class Terraform(object):
             args += ['-var-file', vars_file]
 
         aws_session = self.ctx.provider.session
-
-        args += ['-var', '{}={}'.format('aws_account_id', self.ctx.provider.account_id)]
-        args += ['-var', '{}={}'.format('aws_region', aws_session.region_name)]
-        args += ['-var', '{}={}'.format('aws_profile', self.ctx.provider.profile)]
-        args += ['-var', '{}={}'.format('environment', self.ctx.environment)]
-
         alb = aws_session.client('elbv2')
 
         listeners = {}
@@ -62,21 +64,23 @@ class Terraform(object):
                 key = "{}-{}".format(lb.get("LoadBalancerName"), listener.get("Protocol").lower())
                 listeners[key] = listener.get("ListenerArn")
 
-        args += ['-var', '{}={}'.format('alb_internal_arn', listeners.get("ecs-internal-lb-https"))]
-        args += ['-var', '{}={}'.format('alb_external_arn', listeners.get("ecs-external-lb-https"))]
+        inject_vars = {
+            'aws_account_id': self.ctx.provider.account_id,
+            'aws_region': aws_session.region_name,
+            'aws_profile': self.ctx.provider.profile,
+            'environment': self.ctx.environment,
+            'alb_public_arn_http': listeners.get("public-loadbalancer-http"),
+            'alb_public_arn_https': listeners.get("public-loadbalancer-https"),
 
-        args += ['-var', '{}={}'.format('alb_public_arn', listeners.get("public-loadbalancer-https"))]
-        args += ['-var', '{}={}'.format('alb_public_arn_http', listeners.get("public-loadbalancer-http"))]
+            'alb_external_arn_http': listeners.get("ecs-external-lb-http"),
+            'alb_external_arn_https': listeners.get("ecs-external-lb-https"),
 
-        # new names
-        args += ['-var', '{}={}'.format('alb_public_arn_http', listeners.get("public-loadbalancer-http"))]
-        args += ['-var', '{}={}'.format('alb_public_arn_https', listeners.get("public-loadbalancer-https"))]
+            'alb_internal_arn_http': listeners.get("ecs-internal-lb-http"),
+            'alb_internal_arn_https': listeners.get("ecs-internal-lb-https")
+        }
 
-        args += ['-var', '{}={}'.format('alb_external_arn_http', listeners.get("ecs-external-lb-http"))]
-        args += ['-var', '{}={}'.format('alb_external_arn_https', listeners.get("ecs-external-lb-https"))]
-
-        args += ['-var', '{}={}'.format('alb_internal_arn_http', listeners.get("ecs-internal-lb-http"))]
-        args += ['-var', '{}={}'.format('alb_internal_arn_https', listeners.get("ecs-internal-lb-https"))]
+        for k, v in inject_vars.items():
+            args += ['--var', '{}={}'.format(k, v)]
 
         output = None
         if save:
@@ -87,10 +91,19 @@ class Terraform(object):
 
             output = f.name
 
+        provider_file = join(self.terraform_dir, 'provider.tf')
+        vars = self._get_vars(provider_file).get('variable')
+
+        with open(provider_file, 'a+') as f:
+            for k, v in inject_vars.items():
+                if k in vars:
+                    continue
+                f.write("variable \"{key}\" {{}}\n\n".format(key=k))
+
         return output, self._exec(args)
 
     def apply(self, plan):
-        self._ensure_env(get=False)
+        self._ensure_env()
 
         args = ['apply', plan]
 
@@ -153,7 +166,7 @@ class Terraform(object):
         except Exception:
             return {}
 
-    def _ensure_env(self, get=True):
+    def _ensure_env(self):
         vars_file = join(self.terraform_dir, 'env.{env}.tfvars'.format(env=self.ctx.environment))
 
         if self.ctx.environment == 'local':
@@ -164,10 +177,15 @@ class Terraform(object):
 
         self._exec(['workspace', 'select', self.ctx.environment])
 
-        if get:
-            self._exec(['init', '-upgrade=true'])
-
         return vars_file
+
+    def _update_modules(self, force=False):
+        last_update = os.path.getmtime(self.tfupdate_path) if isfile(self.tfupdate_path) else 0
+
+        if force or int(last_update) < int(time.time()) - 3600:
+            self._exec(['init', '--upgrade=true'])
+            with open(self.tfupdate_path, 'w+') as f:
+                f.write(str(int(time.time())))
 
     def _exec(self, args):
         click.secho("Executing: " + " ".join([self.cmd] + args), fg="blue")
