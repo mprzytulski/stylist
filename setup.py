3a1821ccc204799f50082206c99ba18f941bbe74
Daniel Ancuta <whisller@gmail.com>
2017-11-23 10:11:53 +0000

    Update app version.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='stylist',
-    version='0.4',
+    version='0.5',
     description='One tool to rule them all',
     packages=find_packages(),
     entry_points={
