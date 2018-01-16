70e014f8502538aae9b044b9be99f80a46ed1550
Carlos Cunha <carlos.fontes@threadsstyling.com>
2018-01-16 11:52:00 +0000

    [sentry integration] Enable create proj [WIP #153093288]

--- stylist/provider/sentry.py
+++ stylist/provider/sentry.py
@@ -1,51 +1,51 @@
 import os
 import sys
 import requests
-# import uuid
+import uuid
 
-PRECONDITION_FAILED_STATUS = 412
+from stylist.cli import logger
+
 
 config = {'sentry': {'auth_token': os.environ.get('SENTRY_AUTH_TOKEN')}}
 
-def create_headers(auth_token):
-    return {'Authorization': 'Bearer ' + auth_token}
 
-def get_create_proj_endpoint(host, org_slug, team_slug):
-    endpoint_template = 'https://{}/api/0/teams/{}/{}/projects/'
-    return endpoint_template.format(host, org_slug, team_slug)
-
-def create_proj(auth_token, org_slug, team_slug, proj_name, proj_slug):
-    endpoint = get_create_proj_endpoint('sentry.io', org_slug, team_slug)
-    headers = {'Authorization': 'Bearer ' + auth_token}
-    data = {'name': proj_name, 'slug': proj_slug}
-    response = requests.post(endpoint, data=data, headers=headers)
-    if response.status == 201:
-        return response
+# TODO use this when calling SentryProject
+# logger.error('SENTRY_AUTH_TOKEN environment variable is missing. ' +
+#             "To create the token go to 'https://sentry.io/api/'.")
 
 
-class Sentry:
-    def __init__(self):
-        auth_token = config['sentry']['auth_token']
-        if not auth_token:
-            logger.error('SENTRY_AUTH_TOKEN environment variable is missing. ' +
-                         "To create the token go to 'https://sentry.io/api/'.")
-            sys.exit(PRECONDITION_FAILED_STATUS)
-        self.auth_token = auth_token
+class SentryProject:
 
+    def __init__(self, org_slug, team_slug, host='sentry.io'):
+        if config['sentry']['auth_token']:  # FIXME replace with exception
+            self.auth_token = config['sentry']['auth_token']
+            self.host = host
+            self.org_slug = org_slug
+            self.team_slug = team_slug
+            self.headers = {'Authorization': 'Bearer ' + self.auth_token}
+            self.project = None
+        else:
+            self = None
 
-class SentryOrg(Sentry):
-    def __init__(self, org_slug):
-        Sentry.__init__(self)
-        self.org_slug = org_slug
+    def get_create_proj_endpoint(self):
+        template = 'https://{}/api/0/teams/{}/{}/projects/'
+        return template.format(self.host, self.org_slug, self.team_slug)
 
+    def get_delete_proj_endpoint(self):
+        template = 'https://{}/api/0/projects/{}/{}/'
+        return template.format(self.host, self.org_slug, self.project['slug'])
 
-class SentryTeam(SentryOrg):
-    def __init__(self, team_slug):
-        SentryOrg.__init__(self)
-        self.team_slug = team_slug
+    def create(self, proj_name):
+        proj_slug = proj_name + '_' + uuid.uuid4().hex
+        endpoint = self.get_create_proj_endpoint()
+        data = {'name': proj_name, 'slug': proj_slug}
+        response = requests.post(endpoint, data=data, headers=self.headers)
+        if response.status_code == 201:
+            self.project = response.json()
 
-class SentryProj(SentryTeam):
-    def __init__(self, proj_name, proj_slug):
-        SentryTeam.__init__(self)
-        self.proj_name
-        self.proj_slug
+    def delete(self):
+        if self.project:
+            endpoint = self.get_delete_proj_endpoint()
+            response = requests.delete(endpoint, headers=self.headers)
+            if response.status_code == 204:
+                self.project = None
--- tests/test_sentry.py
+++ tests/test_sentry.py
@@ -1,18 +1,18 @@
-# import pytest
 from unittest import TestCase
-from stylist.cli import logger
-
-def get_delete_proj_endpoint(host, org_slug, proj_slug):
-    endpoint_template = 'https://{}/api/0/projects/{}/{}/'
-    return endpoint_template.format(host, org_slug, proj_slug)
-
-def del_proj():
-    endpoint = get_delete_proj_endpoint(host, org_slug, proj_slug)
+from stylist.provider.sentry import SentryProject
 
 
 class CreateProjTest(TestCase):
+
+    def setUp(self):
+        self.sentry_proj = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
+
+    def test_sentry_project_setup(self):
+        self.assertTrue(self.sentry_proj)
+
     def test_can_create_nonexistent_project(self):
-        proj_slug = 'test_proj_f2857afdaf194eb2b71a4daa4d9d817c'
-        auth_token = config['sentry']['auth_token']
-        created_proj = create_proj(auth_token, 'threads-styling-ltd', 'threads-styling-ltd', 'test_proj', proj_slug)
-        self.assertEqual(created_proj.status, 201)
+        self.sentry_proj.create('test_proj')
+        self.assertTrue(self.sentry_proj.project)
+
+    def tearDown(self):
+        self.sentry_proj.delete()
