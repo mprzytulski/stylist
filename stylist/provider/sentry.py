e63be2a202c0c96e55deb699edbdf8ddfefd5687
Carlos Cunha <carlos.fontes@threadsstyling.com>
2018-01-17 11:48:14 +0000

    [sentry integration] Using static proj slug to create proj [WIP #153093288]

--- stylist/provider/sentry.py
+++ stylist/provider/sentry.py
@@ -1,77 +1,45 @@
 import os
-import sys
 import requests
-import uuid
 
 from stylist.cli import logger
 
 
 config = {'sentry': {'auth_token': os.environ.get('SENTRY_AUTH_TOKEN')}}
 
-
-# TODO use this when calling SentryProject
-# logger.error('SENTRY_AUTH_TOKEN environment variable is missing. ' +
-#             "To create the token go to 'https://sentry.io/api/'.")
-
+# TODO the caller to SentryProject should handle this:
+# raise Exception('SENTRY_AUTH_TOKEN environment variable is missing. ' +
+#                "To create the token go to 'https://sentry.io/api/'.")
 
 class SentryProject:
 
-    def __init__(self, org_slug, team_slug, host='sentry.io'):
-        if config['sentry']['auth_token']:  # FIXME replace with exception
-            self.auth_token = config['sentry']['auth_token']
-            self.host = host
-            self.org_slug = org_slug
-            self.team_slug = team_slug
-            self.headers = {'Authorization': 'Bearer ' + self.auth_token}
-            self.project = None
-        else:
-            self = None
+    def __init__(self, auth_token, org_slug, team_slug, host='sentry.io'):
+        self.host = host
+        self.org_slug = org_slug
+        self.team_slug = team_slug
+        self.headers = {'Authorization': 'Bearer ' + config['sentry']['auth_token']}
 
-    def get_create_proj_endpoint(self):
+    def get_create_proj_endpoint(self, proj_slug):
         template = 'https://{}/api/0/teams/{}/{}/projects/'
         return template.format(self.host, self.org_slug, self.team_slug)
 
-    def get_delete_proj_endpoint(self):
+    def get_delete_proj_endpoint(self, proj_slug):
         template = 'https://{}/api/0/projects/{}/{}/'
-        return template.format(self.host, self.org_slug, self.project['slug'])
+        return template.format(self.host, self.org_slug, proj_slug)
 
-    def get_retrieve_proj_endpoint(self):
-        template = 'https://{}/api/0/organizations/{}/projects/'
-        return template.format(self.host, self.org_slug)
-
-    def list(self):
-        endpoint = self.get_retrieve_proj_endpoint()
-        response = requests.get(endpoint, headers=self.headers)
-        return response.json()
-
-    def list_by_name(self, def_proj_name=None):
-        proj_name = self.project['name'] if def_proj_name is None else def_proj_name
-        is_this_proj = lambda proj: proj_name == proj['name']
-        return list(filter(is_this_proj, self.list()))
-
-    def retrieve(self, proj_name):
-        filtered_projs = self.list_by_name(proj_name)
-        is_not_deleted_proj = lambda proj: proj['status'] != 'deleted'
-        non_deleted_projs = filter(is_not_deleted_proj, filtered_projs)
-        self.project = filtered_projs[0] if non_deleted_projs else None
-        return self.project
-
-    def _create_new(self, proj_name):
-        proj_slug = proj_name + '_' + uuid.uuid4().hex
-        endpoint = self.get_create_proj_endpoint()
-        data = {'name': proj_name, 'slug': proj_slug}
+    def create(self, proj_slug):
+        endpoint = self.get_create_proj_endpoint(proj_slug)
+        data = {'name': proj_slug, 'slug': proj_slug}
         response = requests.post(endpoint, data=data, headers=self.headers)
         if response.status_code == 201:
-            self.project = response.json()
+            logger.info('[Create Sentry Project] OK')
+        elif response.status_code == 409:
+            logger.info('[Create Sentry Project] Already exists. Skipping..')
+        else:
+            exception_msg_template = '[Create Sentry Project] Unexpected issue. Response status code is {}.'
+            raise Exception(exception_msg_template.format(response.status_code), response)
+        return response
 
-    def create(self, proj_name):
-        if not self.retrieve(proj_name):
-            self._create_new(proj_name)
-
-    def delete(self):
-        if self.project:
-            endpoint = self.get_delete_proj_endpoint()
-            response = requests.delete(endpoint, headers=self.headers)
-            if response.status_code == 204:
-                self.project = None
+    def delete(self, proj_slug):
+        endpoint = self.get_delete_proj_endpoint(proj_slug)
+        requests.delete(endpoint, headers=self.headers)
 
