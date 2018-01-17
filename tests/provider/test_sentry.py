e0e7983ba8a7495aee0c1a49122386077f600ef3
Carlos Cunha <carlos.fontes@threadsstyling.com>
2018-01-17 13:32:51 +0000

    [sentry integration] Fix PR change requests  [WIP #153093288]

--- tests/provider/test_sentry.py
+++ tests/provider/test_sentry.py
@@ -1,21 +1,39 @@
+import uuid
+import requests
+
 from unittest import TestCase
 from nose.plugins.attrib import attr
-from stylist.provider.sentry import SentryProject, config
-import uuid
+
+import stylist.provider.sentry as sentry
+
+
+class Sentry(sentry.Sentry):
+
+    def __init__(self, *args, **kwargs):
+        sentry.Sentry.__init__(self, *args, **kwargs)
+
+    def get_delete_proj_endpoint(self, proj_slug):
+        template = 'https://{}/api/0/projects/{}/{}/'
+        return template.format(self.host, self.org_slug, proj_slug)
+
+
+    def delete(self, proj_slug):
+        endpoint = self.get_delete_proj_endpoint(proj_slug)
+        requests.delete(endpoint, headers=self.headers)
 
 
 class CreateProjTest(TestCase):
 
     def setUp(self):
         self.proj_slug = 'test_proj' + '_' + uuid.uuid4().hex
-        self.sentry_proj = SentryProject(config['sentry']['auth_token'],
-                                         'threads-styling-ltd',
-                                         'threads-styling-ltd')
+        self.sentry_proj = Sentry(sentry.config['sentry']['auth_token'],
+                                  'threads-styling-ltd',
+                                  'threads-styling-ltd')
 
     @attr('clutter')
     def test_can_create_project(self):
         response = self.sentry_proj.create(self.proj_slug)
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, requests.codes.created)
 
     @attr('clutter')
     def test_two_projs_same_name_only_one_created_proj(self):
@@ -24,7 +42,7 @@ class CreateProjTest(TestCase):
         successfully_created_proj_status_code = self.sentry_proj.create(self.proj_slug).status_code
         unsuccessfully_created_proj_status_code = self.sentry_proj.create(self.proj_slug).status_code
         self.assertEqual([successfully_created_proj_status_code, unsuccessfully_created_proj_status_code],
-                         [201, 409])
+                         [requests.codes.created, requests.codes.conflict])
 
     def tearDown(self):
         self.sentry_proj.delete(self.proj_slug)
