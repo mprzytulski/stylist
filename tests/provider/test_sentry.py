bc480134086f396fc0df300d66bd62ff5c223df0
Carlos Cunha <carlos.fontes@threadsstyling.com>
2018-01-17 11:48:14 +0000

    [sentry integration] Using static proj slug to create proj [WIP #153093288]

--- tests/provider/test_sentry.py
+++ tests/provider/test_sentry.py
@@ -1,52 +1,31 @@
 from unittest import TestCase
 from nose.plugins.attrib import attr
-from stylist.provider.sentry import SentryProject
+from stylist.provider.sentry import SentryProject, config
+import uuid
 
 
 class CreateProjTest(TestCase):
 
     def setUp(self):
-        self.sentry_proj1 = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
-        self.sentry_proj2 = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
-        self.sentry_proj3 = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
-        self.sentry_proj4 = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
-        self.sentry_proj5 = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
-        self.sentry_proj6 = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
-
-    def test_sentry_project_setup(self):
-        self.assertTrue(self.sentry_proj1)
+        self.proj_slug = 'test_proj' + '_' + uuid.uuid4().hex
+        self.sentry_proj = SentryProject(config['sentry']['auth_token'],
+                                         'threads-styling-ltd',
+                                         'threads-styling-ltd')
 
     @attr('clutter')
     def test_can_create_project(self):
-        self.sentry_proj1._create_new('test_proj')
-        self.assertTrue(self.sentry_proj1.project)
+        response = self.sentry_proj.create(self.proj_slug)
+        self.assertEqual(response.status_code, 201)
 
     @attr('clutter')
-    def test_can_create_two_projects_same_name(self):
-        self.sentry_proj2._create_new('test_proj')
-        self.sentry_proj3._create_new('test_proj')
-        self.assertNotEqual(self.sentry_proj2.project['slug'],
-                            self.sentry_proj3.project['slug'])
-
-    @attr('clutter')
-    def test_can_retrieve_non_deleted_project(self):
-        self.sentry_proj4._create_new('test_proj')
-        self.sentry_proj4.retrieve('test_proj')
-        self.assertTrue(self.sentry_proj4.project)
-        self.assertNotEqual(self.sentry_proj4.project['status'], 'deleted')
-
-    @attr('clutter')
-    def test_can_create_only_one_project_same_name(self):
-        self.sentry_proj5.create('test_proj')
-        self.sentry_proj6.create('test_proj')
-        self.assertEqual(self.sentry_proj5.project['slug'],
-                         self.sentry_proj6.project['slug'])
+    def test_two_projs_same_name_only_one_created_proj(self):
+        # TODO test throwing exception on unknown response status code
+        # TODO test project creation messages being printed
+        successfully_created_proj_status_code = self.sentry_proj.create(self.proj_slug).status_code
+        unsuccessfully_created_proj_status_code = self.sentry_proj.create(self.proj_slug).status_code
+        self.assertEqual([successfully_created_proj_status_code, unsuccessfully_created_proj_status_code],
+                         [201, 409])
 
     def tearDown(self):
-        self.sentry_proj1.delete()
-        self.sentry_proj2.delete()
-        self.sentry_proj3.delete()
-        self.sentry_proj4.delete()
-        self.sentry_proj5.delete()
-        self.sentry_proj6.delete()
+        self.sentry_proj.delete(self.proj_slug)
 
