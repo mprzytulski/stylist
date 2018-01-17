e920f41f54ba7be3a37ef871528aa1995443b0d2
Carlos Cunha <carlos.fontes@threadsstyling.com>
2018-01-17 09:40:30 +0000

    [sentry integration]  Fix SentryProj to retrieve a non deleted proj [WIP #153093288]

--- tests/provider/test_sentry.py
+++ tests/provider/test_sentry.py
@@ -8,6 +8,10 @@ class CreateProjTest(TestCase):
     def setUp(self):
         self.sentry_proj1 = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
         self.sentry_proj2 = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
+        self.sentry_proj3 = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
+        self.sentry_proj4 = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
+        self.sentry_proj5 = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
+        self.sentry_proj6 = SentryProject('threads-styling-ltd', 'threads-styling-ltd')
 
     def test_sentry_project_setup(self):
         self.assertTrue(self.sentry_proj1)
@@ -19,19 +23,30 @@ class CreateProjTest(TestCase):
 
     @attr('clutter')
     def test_can_create_two_projects_same_name(self):
-        self.sentry_proj1._create_new('test_proj')
         self.sentry_proj2._create_new('test_proj')
-        self.assertNotEqual(self.sentry_proj1.project['slug'],
-                            self.sentry_proj2.project['slug'])
+        self.sentry_proj3._create_new('test_proj')
+        self.assertNotEqual(self.sentry_proj2.project['slug'],
+                            self.sentry_proj3.project['slug'])
+
+    @attr('clutter')
+    def test_can_retrieve_non_deleted_project(self):
+        self.sentry_proj4._create_new('test_proj')
+        self.sentry_proj4.retrieve('test_proj')
+        self.assertTrue(self.sentry_proj4.project)
+        self.assertNotEqual(self.sentry_proj4.project['status'], 'deleted')
 
     @attr('clutter')
     def test_can_create_only_one_project_same_name(self):
-        self.sentry_proj1.create('test_proj')
-        self.sentry_proj2.create('test_proj')
-        self.assertEqual(self.sentry_proj1.project['slug'],
-                         self.sentry_proj2.project['slug'])
+        self.sentry_proj5.create('test_proj')
+        self.sentry_proj6.create('test_proj')
+        self.assertEqual(self.sentry_proj5.project['slug'],
+                         self.sentry_proj6.project['slug'])
 
     def tearDown(self):
         self.sentry_proj1.delete()
         self.sentry_proj2.delete()
+        self.sentry_proj3.delete()
+        self.sentry_proj4.delete()
+        self.sentry_proj5.delete()
+        self.sentry_proj6.delete()
 
